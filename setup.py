c7463c08df15fe809245ec11b7484f73365785ef
Hunter Senft-Grupp <hsenftgrupp@athenahealth.com>
2017-05-21 08:36:54 -0700

    This is going to be major version 1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='slackly',
-    version='0.1.0',
+    version='1.0.0',
     install_requires=[
         'requests',
         'websocket-client',
