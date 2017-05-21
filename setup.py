19e734c5aa8386e0d397a1761b57374761f36da7
Hunter Senft-Grupp <huntcsg@gmail.com>
2017-05-21 12:37:44 -0700

    Added classifiers and metadata

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 try:
     with open(os.path.join(here, 'README.rst')) as f:
-        readme = f.read()
+        README = f.read()
     with open(os.path.join(here, 'CHANGES.txt')) as f:
         CHANGES = f.read()
+    with open(os.path.join(here, 'AUTHORS.rst')) as f:
+        AUTHORS = f.read()
 except IOError:
-    readme = changes = ''
+    README = CHANGES = AUTHORS = ''
 
 
 
@@ -19,7 +21,7 @@ install_requires = [
 
 docs_require = [
     'sphinx',
-    'rtd_theme',
+    'sphinx_rtd_theme',
 ]
 
 testing_requires = [
@@ -31,6 +33,12 @@ testing_requires = [
 setup(
     name='slackly',
     version='1.0.0',
+    description='A full featured python Slack API SDK',
+    long_description="\n".join([README, CHANGES, AUTHORS]),
+    author='Hunter Senft-Grupp',
+    author_email='huntcsg@gmail.com',
+    url='https://slackly.io',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=install_requires,
     extras_require={
         'docs': docs_require,
@@ -41,4 +49,22 @@ setup(
         '': 'src',
     },
     packages=find_packages('src'),
+    keywords="slack slackly api sdk realtime",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Communications :: Chat',
+        'Topic :: Office/Business',
+        'Topic :: Software Development :: Libraries'
+    ],
 )
