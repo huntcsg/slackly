d5233f14c836258fd3551392510c387b63a99bc9
Hunter Senft-Grupp <huntcsg@gmail.com>
2017-05-24 14:51:48 -0700

    A startup script and a better demo

---
+++ src/slackly/start_here/__main__.py
@@ -0,0 +1,16 @@
+import argparse
+import code
+import slackly
+import sys
+
+
+try:
+    parser =argparse.ArgumentParser(prog="slackly.start_here", description="")
+    parser.add_argument("token", type=str, help="A Slack API Token. Not a Client Secret.")
+    options = parser.parse_args()
+
+    client = slackly.SlackClient(token=options.token, response_factory=slackly.SlackAPIObjectResponse)
+    code.interact(banner="", local=locals())
+
+except SystemExit:
+    print("Use Ctrl-D to exit")
