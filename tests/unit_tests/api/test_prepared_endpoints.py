690a8eb8e785ec2ce61b1425d26682633b9af45a
Hunter Senft-Grupp <hsenftgrupp@athenahealth.com>
2017-05-24 09:22:21 -0700

    Added unit tests for all endpoints with required and optional args

---
+++ tests/unit_tests/api/test_prepared_endpoints.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from slackly.api.endpoints._base import BaseAPIEndpoint, BaseAPIDispatch, SlackAPICall
+
+
+def call_end_points(dispatcher):
+    calls = []
+    for attr in vars(dispatcher.__class__).values():
+        if isinstance(attr, BaseAPIEndpoint):
+            fake_kwargs = {arg: 'DUMMY' for arg in attr.required_args}
+            fake_optional_kwargs = {arg: 'DUMMY' for arg in attr.optional_args}
+            fake_optional_kwargs.update(fake_kwargs)
+            # Make the prepared call
+
+            calls.append((
+                attr.endpoint,
+                lambda self, attr=attr, fake_kwargs=fake_kwargs: self.assertIsInstance(attr(**fake_kwargs), SlackAPICall),
+                lambda self, attr=attr, fake_optional_kwargs=fake_optional_kwargs: self.assertIsInstance(attr(**fake_optional_kwargs), SlackAPICall)
+            ))
+
+        elif isinstance(attr, BaseAPIDispatch):
+            calls.extend(call_end_points(attr))
+
+    return calls
+
+class TestPreparedCalls(TestCase):
+
+    from slackly import SlackAPI
+    api = SlackAPI()
+
+    calls = []
+    calls.extend(call_end_points(api))
+    for endpoint, callable_test_required_args, callable_test_optional_args in calls:
+        endpoint_str = endpoint.replace('.', '_')
+        locals()["test_prepared_call_required_only_{}".format(endpoint)] = callable_test_required_args
+        locals()["test_prepared_call_optional_{}".format(endpoint)] = callable_test_optional_args
