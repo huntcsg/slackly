53b4e4de050ccab93c0d692fe50e7e852e159329
Hunter Senft-Grupp <huntcsg@gmail.com>
2017-06-03 21:09:35 -0400

    Added a deploy script to do the pypi deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,59 @@
 #!/usr/bin/env bash
 
+# Let's die if we have an error
 set -e
 
-# This assumes you have a master slackly directory
-# that contains slackly and slackly_utils
-# and has a virtual environment called "venv"
-source ../venv/bin/activate
+cd ../deploy
 
-# Get the current (working) version
-git checkout master
-git pull origin master
+# Activate the build/deploy virtualenv
+source venv/bin/activate
+
+# Install the two things we need to make this work
+pip install --upgrade bumpversion twine >> /dev/null 2>&1
+
+# Get rid of the "new" directory
+if [ -e new ];
+then
+    echo "Remove directory <new>"
+    rm -rf new
+fi
+
+echo "Cloning slackly"
+git clone https://github.com/huntcsg/slackly.git new >> /dev/null 2>&1
+
+cd new
+
+# Get the old version from the setup.cfg
+OLD_VERSION=`python -c "import configparser; c=configparser.RawConfigParser(); c.read('setup.cfg'); print(c.get('bumpversion', 'current_version'));"`
+
+# We are going to bump the version and then
+# push to the origin repo
+echo "Bumping version ($1)"
 bumpversion $1
-python setup.py sdist bdist_wheel
 
-CURRENT_VERSION=`python -c "import slackly; print(slackly.__version__)"`
+# Let's build the package and upload it to pypi
+python setup.py sdist bdist_wheel  >> /dev/null 2>&1
 
-twine upload sdist/slackly-${CURRENT_VERSION}*
+CURRENT_VERSION=`python -c "import configparser; c=configparser.RawConfigParser(); c.read('setup.cfg'); print(c.get('bumpversion', 'current_version'));"`
+
+echo "Uploading to twine"
+twine upload dist/slackly-${CURRENT_VERSION}*
+
+# If we succeeded, let's push to github with the new commits and tags
+echo "Pushing to github with new version and tag"
+git push --tags origin master
+
+
+cd ..
+
+echo "Shuffling directories"
+if [ -e latest ];
+then
+    if [ -e "$OLD_VERSION" ];
+    then
+        echo "old version <$OLD_VERSION> already saved. Something probably went wrong."
+    else
+        mv latest $OLD_VERSION
+    fi
+fi
+mv new latest
